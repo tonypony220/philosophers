6f77e6bfe501295993f76eb81546dfb72f864385
tonypony220 <61715244+tonypony220@users.noreply.github.com>
2021-07-21 13:11:19 +0300

    d

--- philo.c
+++ philo.c
@@ -1,13 +1,53 @@
 
-struct vars 
+int	ft_atoi(char *str)
 {
-    pthread_mutex_t **mutex;
+	int		sign;
+	long	num;
+
+	num = 0;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	sign = 1 - 2 * (*str == '-' && 1);
+	*str == '-' && str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		num = num * 10 + *str++ - '0';
+		if (num < 0)
+			return ((sign > 0) * -1);
+	}
+	return ((int)num * sign);
+}
+
+struct s_vars 
+{
+    pthread_mutex_t **mutexes;
 	die				int;
 	eat				int;
 	sleep			int;
 	meal_num		int;
 };
 
+/* i: philo number */
+void *worker_thread(void *v_data)
+{
+	while ()
+	{
+        pthread_mutex_lock(mutexes);
+        pthread_mutex_unlock(mutexes);
+    }
+    return NULL;  /* never reached */
+}
+
+
+int lunch(struct s_vars *var, int n)
+{
+	var->mutexes = malloc(sizeof(pthread_mutex_t) * (n + 1));
+	memset(var->mutexes, 0, sizeof(pthread_mutex_t) * (n + 1));
+	while (n--)	
+	{
+		var->mutexes[n] = PTHREAD_MUTEX_INITIALIZER;
+	}
+}
 
 /* args:
  * 		1 number_of_philosophers
@@ -16,25 +56,15 @@ struct vars
  * 		4 time_to_sleep 
  * 		5 number_of_times_each_philosopher_must_eat
  */
-int lunch(struct vars *var, int n)
-{
-	while (n--)	
-	{
-
-	}
-}
-
 int p_main(int ac, char **av)
 {
-	struct vars var;
+	struct s_vars var;
 
-	var.die = av[2]
-	var.eat = av[3]
-	var.sleep = av[4]
+	var.die = ft_atoi(av[2]);
+	var.eat = ft_atoi(av[3]);
+	var.sleep = ft_atoi(av[4]);
 	var.meal_num = 0;
-	ac == 6 && (var.meal_num = av[5]);
-		
-	var.mutex = PTHREAD_MUTEX_INITIALIZER;
+	ac == 6 && (var.meal_num = ft_atoi(av[5]));
+	lunch(&var, ft_atoi(av[1]));	
 	return(_main(ac, av));
 }
-
